a4e353b2d1d2fdc5a805c5f5e24e5cda920ad5fe
DELL <DELL@DESKTOP-M7BIGAP>
2022-03-02 15:23:48 +0530

    project of US

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello 
+{
+
+	public static void main(String[] args) 
+	{
+		int b =15;
+		System.out.println("in main");
+		
+		for(int a=1;a<=b;a++)
+		{
+			System.out.println("Java");
+		}
+		System.out.println("out main");
+	}
+
+}
---
+++ src/Java.java
@@ -0,0 +1,18 @@
+
+public class Java {
+
+	public static void main(String[] args) 
+	{
+		int a = 9;
+		
+		if(a%2==0)
+		{
+			System.out.println("No is Even");
+		}
+		else
+		{
+			System.out.println("No is Odd");
+		}
+	}
+
+}
